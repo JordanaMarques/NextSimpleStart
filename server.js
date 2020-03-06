33a5005221b1d3741ed2384d980b39b1208ed94f
JordanaMarques <jojoo.makes@gmail.com>
2020-03-06 09:43:37 +0000

    routes and item-controller created

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-var logger = require ("morgan"),
-cors = require ("cors"),
-http = require ("http"),
-express = require ("express"),
-bodyParser = ("body-parser");
+var logger = require("morgan"),
+cors = require("cors"),
+http = require("http"),
+express = require("express"),
+bodyParser = require("body-parser");
 
 var app = express();
 var port = 3000;
 
-app.get ('/hello', function (req, res){
-    res.send("Hello World");
+app.use(logger('dev'));
+app.use(bodyParser.json());
+
+app.get('/hello/:foo/:bar', function(req,res){
+    res.json({result: "Hello World! 😤😱🥵🤡", data: [
+        req.params.foo,
+        req.params.bar
+    ]});
+});
+
+app.post('/hello', function(req,res){
+    res.json({result: "The POST request was sent", data: req.body});
+
 });
 
 app.listen(port, function(err){
-    console.log("Listening on port:" + port);
+    console.log("Listening on Port: " + port)
 });
